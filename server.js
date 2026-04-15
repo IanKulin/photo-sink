fdeb50389387a50626e946bd6f5ba8a4279efd2b
IanKulin <IanKulin@kulin.com.au>
2026-04-15 14:25:09 +0800

    nice favicons

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const app = require("./app");
 const logger = require("./src/logger");
+const { db } = require("./src/db");
 
 const port = process.env.PORT || 3000;
 
 logger.ready.then(() => {
-  app.listen(port, () => {
+  const server = app.listen(port, () => {
     logger.info("photo-sink listening on port %d", port);
   });
+
+  function shutdown(signal) {
+    logger.info("received %s, shutting down", signal);
+    server.close(() => {
+      db.close();
+      logger.info("shutdown complete");
+      process.exit(0);
+    });
+  }
+
+  process.on("SIGTERM", () => shutdown("SIGTERM"));
+  process.on("SIGINT", () => shutdown("SIGINT"));
 });
